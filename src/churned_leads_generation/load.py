5db1ae0cae67dc4e176652c00ae836a20334a325
Dhandre <AL32267@LV54677RLJ2.US.AD.WELLPOINT.COM>
2023-04-21 16:29:30 -0400

    CC1120-lead-churns

--- src/churned_leads_generation/load.py
+++ src/churned_leads_generation/load.py
@@ -2,25 +2,20 @@ import pandas as pd
 import json
 from queries.sql_queries import INSERT_LEAD_CHURNS_DATA_SQL
 
+def insert_lead_churns(engine):
+    print("Churned leads Insert")
+    conn = engine.connect()
+    conn.execute(INSERT_LEAD_CHURNS_DATA_SQL)
+    return None
 
-class ChurnedLeads:
-    def __init__(self):
-        self.engine = engine
-        self.job_type = 'churned_lead_generation'
-        self.extract_date = extract_date
-        self.des = 'Incremental load extract for Churned Lead Generation'
-        self.flag = 'Y'
+def upd_inc_job_cntl(engine) -> None:
+    job_type = 'churned_lead_generation'
+    extract_date = extract_date
+    des = 'Incremental load extract for Churned Lead Generation'
+    flag = 'Y'
+    query = "INSERT INTO incremental_job_cntl(job_type,last_extract_dt,description,created_at,flag) VALUES (%s,%s,%s,%s,%s)"
 
-    def insert_lead_churns(self):
-        print("Churned leads Insert")
-        conn = self.engine.connect()
-        conn.execute(INSERT_LEAD_CHURNS_DATA_SQL)
-        return None
-
-    def upd_inc_job_cntl(self) -> None:
-        query = "INSERT INTO incremental_job_cntl(job_type,last_extract_dt,description,created_at,flag) VALUES (%s,%s,%s,%s,%s)"
-        engine = self.engine()
-        with engine.begin() as conn:  # TRANSACTION
-            #print("insert")
-            conn.execute(query, (self.job_type, self.extract_date, self.des, datetime.now(), self.flag,))
-            #conn.execute(query)
+    with engine.begin() as conn:  # TRANSACTION
+        #print("insert")
+        conn.execute(query, (job_type, extract_date, des, datetime.now(), flag))
+        #conn.execute(query)
--- src/churned_leads_generation/main.py
+++ src/churned_leads_generation/main.py
@@ -1,10 +1,9 @@
 import boto3
 import os
 import pandas as pd
-#import load
 from utility.db import make_engine
 from utility.utils import parse_envs, create_logger
-from load import ChurnedLeads
+from load import insert_lead_churns
 
 
 os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
@@ -23,7 +22,7 @@ def execute():
         engine = make_engine(db_env=DB_ENV)
 
         # Insert model_updates
-        ChurnedLeads.insert_lead_churns()
+        insert_lead_churns(engine=engine)
 
     except Exception as e:
         logger.error(f"EXCEPTION! {e}")
