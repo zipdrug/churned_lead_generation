9523103f9c12e9de601b476a40130c0d0c16d80e
Dhandre <AL32267@LV54677RLJ2.US.AD.WELLPOINT.COM>
2023-04-21 14:53:54 -0400

    CC1120-lead-churns

--- src/churned_leads_generation/main.py
+++ src/churned_leads_generation/main.py
@@ -1,9 +1,10 @@
 import boto3
 import os
 import pandas as pd
+import load
 from utility.db import make_engine
 from utility.utils import parse_envs, create_logger
-from load import insert_lead_churns, upd_inc_job_cntl
+#from load import insert_lead_churns, upd_inc_job_cntl
 
 
 os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
