6e69dc4c23b9be5280181553b21b98366d8a7dd4
Dhandre <AL32267@LV54677RLJ2.US.AD.WELLPOINT.COM>
2023-04-25 12:27:58 -0400

    CC1120-lead-churns

--- src/churned_leads_generation/load.py
+++ src/churned_leads_generation/load.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import json
 from datetime import datetime
-from queries.sql_queries import INSERT_LEAD_CHURNS_DATA_SQL, UPDATE_INCREMENTAL_CONTROL_TABLE_SQL, CHECK_EXISTING_PATIENT_SQL, GET_PATIENT_ID_SQL, INSERT_TABLE_DATA_SQL, UPDATE_LEAD_CHURNS_SQL
+from queries.sql_queries import GET_CHURNED_LEADS_DATA_SQL, CHECK_EXISTING_PATIENT_SQL,UPDATE_LEAD_CHURNS_SQL,  INSERT_LEAD_CHURNS_SQL, UPDATE_INCREMENTAL_CONTROL_TABLE_SQL
 
+
+# Function to fetch Churned Leads Data, Check if already exists in table then update if not Make a new entry into lead_churns
 def insert_lead_churns(engine) -> None:
     print("Checking if patient_id already exists in lead_churns table...")
-    query = INSERT_LEAD_CHURNS_DATA_SQL
+    query = GET_CHURNED_LEADS_DATA_SQL
     patient_id_df = pd.read_sql(sql=query, con=engine)
-    print("patient_id_df", patient_id_df)
     len_patient_id_df = len(patient_id_df)
 
     if len_patient_id_df > 0:
-        #patient_id_df = pd.read_sql(sql=query, con=engine)
         print("Checking if patient_id's already exists in table...")
         for ind in patient_id_df.index:
-            print("loop:", ind)
-            print("value", patient_id_df['patient_id'][ind])
+            print("loop:", ind, "patient_id:", patient_id_df['patient_id'][ind])
             check_query = CHECK_EXISTING_PATIENT_SQL.format(patient_id=patient_id_df['patient_id'][ind])
             exist_df = pd.read_sql(sql=check_query, con=engine)
             if len(exist_df) > 0:
@@ -25,27 +24,22 @@ def insert_lead_churns(engine) -> None:
                 conn.execute(update_query)
             else:
                 print("New entry for churned leads")
-                insert_query = INSERT_TABLE_DATA_SQL.format(fname=patient_id_df['first_name'][ind], lname=patient_id_df['last_name'][ind], patient_id=patient_id_df['patient_id'][ind])
+                insert_query = INSERT_LEAD_CHURNS_SQL.format(fname=patient_id_df['first_name'][ind], lname=patient_id_df['last_name'][ind], patient_id=patient_id_df['patient_id'][ind])
                 conn = engine.connect()
                 conn.execute(insert_query)
-        return None
+        #return None
 
     else:
         print("No entries to insert for churned leads..")
-    #print("Churned leads Insert")
-    #conn = engine.connect()
-    #conn.execute(INSERT_LEAD_CHURNS_DATA_SQL)
-    #return patient_id_df
 
+
+# Function to make job run entry into incremental_job_cntl
 def upd_inc_job_cntl(engine) -> None:
     job_type = 'churned_lead_generation'
     des = 'Incremental load extract for Churned Lead Generation'
     flag = 'Y'
     query = UPDATE_INCREMENTAL_CONTROL_TABLE_SQL.format(job_type=job_type, description=des, flag=flag)
-    #query = "INSERT INTO incremental_job_cntl(job_type,last_extract_dt,description,created_at,flag) VALUES (%s,%s,%s,%s,%s)"
 
     with engine.begin() as conn:  # TRANSACTION
         print("Updating incremental job control table")
         conn.execute(query)
-        #conn.execute(UPDATE_INCREMENTAL_CONTROL_TABLE_SQL, (job_type, datetime.now(), des, datetime.now(), flag))
-        #conn.execute(query)
--- src/churned_leads_generation/queries/sql_queries.py
+++ src/churned_leads_generation/queries/sql_queries.py
@@ -1,51 +1,4 @@
-GET_PATIENT_ID_SQL="""
-with job_cnt as (
-select
-max(last_extract_dt) as last_extract_dt
-from
-incremental_job_cntl ijc
-where
-job_type = 'churned_lead_generation'
-and flag = 'Y')
-
-,patients_ext as (
-select 
-p.id as patient_id,
-cast(p.enroll_status as varchar(255)) as enroll_status
-from patients p 
-
-)
-
-,model_update as (
-SELECT 
-patient_id,
-created_at,
-updated_at, 
-Replace(cast(new_fields -> 'enroll_status' AS varchar(255)),'"','')  as enroll_status
-from
-model_updates mu 
-where
-cast (new_fields -> 'enroll_status' as varchar(255)) = '"churned"'
-)
-
-select 
-p.patient_id as patient_id
-from 
-model_update mu
-join patients_ext p on p.patient_id= mu.patient_id
-join job_cnt jc on mu.created_at > jc.last_extract_dt
-where mu.created_at > jc.last_extract_dt
-and mu.enroll_status = p.enroll_status
-"""
-
-
-CHECK_EXISTING_PATIENT_SQL = """
-SELECT PATIENT_ID FROM LEAD_CHURNS LC WHERE PATIENT_ID = '{patient_id}'
-"""
-
-UPDATE_LEAD_CHURNS_SQL = """ UPDATE LEAD_CHURNS SET CREATED_AT = NOW(), UPDATED_AT = NOW() WHERE PATIENT_ID = '{patient_id}' """
-
-INSERT_LEAD_CHURNS_DATA_SQL = """
+GET_CHURNED_LEADS_DATA_SQL = """
 --insert into lead_churns (first_name,last_name,patient_id,assigned_user_id,filtered_out,filtered_by_user_id,created_at,updated_at,deleted_at)
 with job_cnt as (
 select
@@ -85,7 +38,7 @@ first_name,
 last_name,
 p.patient_id as patient_id,
 '182' as assigned_user_id,
-null as filtered_out,
+null1 as filtered_out,
 null as filtered_by_user_id,
 now() as created_at,
 now() as updated_at,
@@ -98,7 +51,13 @@ where mu.created_at > jc.last_extract_dt
 and mu.enroll_status = p.enroll_status
 """
 
-INSERT_TABLE_DATA_SQL = """
+CHECK_EXISTING_PATIENT_SQL = """
+SELECT PATIENT_ID FROM LEAD_CHURNS LC WHERE PATIENT_ID = '{patient_id}'
+"""
+
+UPDATE_LEAD_CHURNS_SQL = """ UPDATE LEAD_CHURNS SET CREATED_AT = NOW(), UPDATED_AT = NOW() WHERE PATIENT_ID = '{patient_id}' """
+
+INSERT_LEAD_CHURNS_SQL = """
 INSERT INTO LEAD_CHURNS (FIRST_NAME,LAST_NAME,PATIENT_ID,ASSIGNED_USER_ID,FILTERED_OUT,FILTERED_BY_USER_ID,CREATED_AT,UPDATED_AT,DELETED_AT)
 VALUES ('{fname}','{lname}','{patient_id}','182',NULL, NULL,NOW(),NOW(),NULL) """
 
